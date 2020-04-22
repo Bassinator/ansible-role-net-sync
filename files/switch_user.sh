2932e705314fbc1c676f54a08b46a170379c27c9
Bastian Bukatz <bastian.greuel@gmail.com>
2020-04-22 16:45:04 +0200

    removed auto backup and restore from switch user script to prevent accidential data loss

--- files/switch_user.sh
+++ files/switch_user.sh
@@ -5,12 +5,8 @@ if [ ! "$#" -eq 1 ]
   exit 1
 fi
 
-backup_home.sh
-
 host_name=$1
 echo $host_name | sudo tee /etc/hostname
-sudo sed -i -E 's/^127.0.1.1.*/127.0.1.1\t'"$host_name"'/' /etc/hosts
-sudo hostnamectl set-hostname $host_name
-sudo systemctl restart avahi-daemon
-
-restore_home.sh
+sed -i -E 's/^127.0.1.1.*/127.0.1.1\t'"$host_name"'/' /etc/hosts
+hostnamectl set-hostname $host_name
+systemctl restart avahi-daemon
