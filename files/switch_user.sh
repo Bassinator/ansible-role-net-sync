7dd04a5af520e1addb56bad5e3eacf7d7edb72ff
Bastian Bukatz <bastian.greuel@gmail.com>
2020-04-22 18:32:56 +0200

    modify .bashrc to reflect hostname change in terminal prompt immedialty

--- files/switch_user.sh
+++ files/switch_user.sh
@@ -5,6 +5,12 @@ if [ ! "$#" -eq 1 ]
   exit 1
 fi
 
+if [[ $(avahi-relolve-host-name $1.local) =~ ^"$1" ]]
+then
+  echo error: hostname $1 already in use in your local network
+  exit 1
+fi
+
 host_name=$1
 echo $host_name | sudo tee /etc/hostname
 sed -i -E 's/^127.0.1.1.*/127.0.1.1\t'"$host_name"'/' /etc/hosts
